87fd21c1d24e5610f269a38732af00cd2dbbc1bf
Dilan Akdogan <dakdogan@schueler.hakzell.at>
2020-06-05 08:24:35 +0200

    Zahlen können eingegeben werden

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rechner.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,11 +12,29 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             // Verwendung der Klasse
+            Rechenautomat rechner = new Rechenautomat();
 
             string eingabe = "-";
             while (eingabe != "")
             {
                 // Benutzereingabe - Achtung: Exception möglich
+                Console.Write("Eingabe: ");
+                eingabe = Console.ReadLine();
+
+                // nur wenn eine Eingabe vorhanden ist
+                if (eingabe != "")
+                {
+                    try
+                    {
+                        int zahl = Convert.ToInt32(eingabe);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Bitte geben Sie nur Zahlen ein.");
+                    }
+                }
+
+                
 
             }
         }
