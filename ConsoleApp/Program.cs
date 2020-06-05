16fad9b146026822d8e9a6c0dc8908f770527098
Dilan Akdogan <dakdogan@schueler.hakzell.at>
2020-06-05 08:13:17 +0200

    add libary

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Verwendung der Klasse
+
+            string eingabe = "-";
+            while (eingabe != "")
+            {
+                // Benutzereingabe - Achtung: Exception möglich
+
+            }
+        }
+    }
+}
